20ef3818fadd2bf02bc2c9cc67a393e47b92c304
105cbingqilin <95478849+105cbingqilin@users.noreply.github.com>
2023-09-21 19:07:41 -0300

    Set up a basic layout of the site

---
+++ script.js
@@ -0,0 +1,6 @@
+for (let i = 0; i < 16; i++) {
+    const container = document.querySelector('.grid')
+    const content = document.createElement('div');
+    content.classList.add('box');
+    container.appendChild(content);
+}
